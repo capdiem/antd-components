0fc2fea931d5fc8c28bc4a1bde9414468f628c92
capdiem <capdiem@live.com>
2019-11-18 16:02:50 +0800

    :wrench: chore: gitignore lib

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-declare const _default: {
-    SubstringText: import("react").FC<import("./substring-text").SubstringTextComponentProps>;
-};
-export default _default;
--- lib/substring-text/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export interface SubstringTextComponentProps {
-    text: string;
-    width: number;
-    row?: number;
-    separator?: string;
-}
-declare const SubstringText: React.FC<SubstringTextComponentProps>;
-export default SubstringText;
