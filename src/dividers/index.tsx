bb187c9cc5dda5e81fc81c1072b1c02d51db3dc0
capdiem <capdiem@live.com>
2019-12-09 16:34:16 +0800

    :bug: fix: Update Dividers: add :hover to rowStyle, add dividerVisible ability

--- src/dividers/index.tsx
+++ src/dividers/index.tsx
@@ -1,12 +1,14 @@
 import "antd/es/divider/style";
 
 import Divider, { DividerProps } from "antd/es/divider";
+import Radium from "radium";
 import React from "react";
 
 export interface DividerComponentProps {
   rows: React.ReactNode[];
   type?: DividerProps["type"];
   style?: React.CSSProperties;
+  dividerVisible?: boolean;
   dividerStyle?: React.CSSProperties;
   rowStyle?: React.CSSProperties;
 }
@@ -16,6 +18,7 @@ const Dividers: React.FC<DividerComponentProps> = ({
   type = "horizontal",
   style = {},
   dividerStyle = {},
+  dividerVisible = true,
   rowStyle = {},
 }) => {
   const _dividerStyle: React.CSSProperties =
@@ -24,18 +27,29 @@ const Dividers: React.FC<DividerComponentProps> = ({
   const _style: React.CSSProperties =
     type === "vertical" ? { display: "flex", justifyContent: "center" } : {};
 
+  const _rowStyle: Radium.StyleProps['rules'] = {
+    ":hover": {
+      opacity: 0.7,
+    },
+    ...rowStyle,
+  };
+
   return (
     <div style={{ textAlign: "center", ..._style, ...style }}>
       {rows
         .filter((row) => row !== undefined && row !== null && row !== false)
         .map((row, index) => (
-          <div key={index} style={rowStyle}>
-            {index !== 0 && <Divider style={{ ..._dividerStyle, ...dividerStyle }} type={type} />}
-            {row}
+          <div key={index}>
+            {index !== 0 && dividerVisible && (
+              <Divider style={{ ..._dividerStyle, ...dividerStyle }} type={type} />
+            )}
+            <span style={_rowStyle} key={index}>
+              {row}
+            </span>
           </div>
         ))}
     </div>
   );
 };
 
-export default Dividers;
+export default Radium(Dividers);
