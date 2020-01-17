d26f56c846ff943a5aa1e11e692ed09142da4bc9
capdiem <capdiem@live.com>
2020-01-17 13:17:28 +0800

    :bug: fix: Customize dataIndex type

--- src/table/types.ts
+++ src/table/types.ts
@@ -1,12 +1,14 @@
 import { ColumnType } from "antd/lib/table/interface";
 import { TableProps } from "antd/lib/table/Table";
 
-export type TableColumnProps<T> = Omit<ColumnType<T>, "children"> & {
+export type TableColumnProps<T = any> = Omit<ColumnType<T>, "children" | "dataIndex"> & {
   visible?: boolean;
+  /** TODO: How to get nested object keys? */
+  dataIndex?: keyof T | [keyof T, ...string[]] | number | number[];
   children?: TableColumnProps<T>[];
 };
 
-export type TableComponentProps<T> = Omit<TableProps<T>, "rowKey" | "columns" | "scroll"> & {
+export type TableComponentProps<T = any> = Omit<TableProps<T>, "rowKey" | "columns" | "scroll"> & {
   fullscreen?: boolean;
   fullscreenWidth?: string | number;
   rowKey?: keyof T | (keyof T)[] | ((record: T) => string);
