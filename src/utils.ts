6864af38965644a385a11dbf0968386af308bcd2
capdiem <capdiem@live.com>
2020-02-15 19:36:30 +0800

    :sparkles: feat: Add flatten function

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,18 @@ export function computeCell(count: number) {
   if (count === 3) return 8;
   return 6;
 }
+
+/**
+ * flatten
+ * @param array
+ * @example const arr = [...flatten([1,2,[3,4,[5,6]]])]
+ */
+export function* flatten(array: any[]) {
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      yield* flatten(item);
+    } else {
+      yield item;
+    }
+  }
+}
