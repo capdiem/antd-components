0e0af6edd307b5cb4fc618f0a1ea2ec4d45c16f5
capdiem <capdiem@live.com>
2020-05-28 17:49:09 +0800

    :new: feat: Dividers support multi rows(rows groups)

--- src/dividers/index.tsx
+++ src/dividers/index.tsx
@@ -19,20 +19,30 @@ export interface Row {
   onClick?: () => void;
 }
 
+export type Rows = React.ReactNode[] | Row[];
+export type RowsGroups = Array<Rows>;
+
 export interface DividerComponentProps {
-  rows: React.ReactNode[] | Row[];
+  rows: Rows | RowsGroups;
   type?: DividerProps["type"];
   style?: React.CSSProperties;
   dividerVisible?: boolean;
   dividerStyle?: React.CSSProperties;
+  outerDividerVisible?: boolean;
+  outerDividerStyle?: React.CSSProperties;
   rowStyle?: React.CSSProperties;
 }
+
+declare type SizeType = "icon" | "label" | "full";
 
 function isRow(row: Row | React.ReactNode): row is Row {
   return Boolean(row) && (row as Row).label !== undefined;
 }
 
 const marginLeft = 4;
+const iconIndex = 0;
+const labelIndex = 1;
+const fullIndex = 2;
 
 const Dividers: React.FC<DividerComponentProps> = ({
   rows,
@@ -40,88 +50,114 @@ const Dividers: React.FC<DividerComponentProps> = ({
   style = {},
   dividerStyle = {},
   dividerVisible = true,
+  outerDividerStyle = {},
+  outerDividerVisible = true,
   rowStyle = {},
 }) => {
   const [size, divRef] = useSize<HTMLDivElement>();
-  const widthRef = useRef<{ icon?: number; label?: number; full?: number }>({});
-  const [sizeType, setSizeType] = useState<"icon" | "label" | "full">();
+  const widthRef = useRef<number[][]>([]);
+  const [sizeType, setSizeType] = useState<Array<SizeType>>([]);
+
+  function computeVertical(rows: Rows) {
+    let iconPx = 0;
+    let labelPx = 0;
+    let fullPx = 0;
+
+    (rows as Row[])
+      .filter((u) => Object.keys(u).indexOf("visible") === -1 || u.visible)
+      .forEach((u, i) => {
+        /** icon width */
+        if (u.icon) {
+          iconPx += 14;
+          if (i > 0) iconPx += 17;
+
+          fullPx += 14;
+        }
+
+        /** divider width */
+        if (i > 0) {
+          labelPx += 17;
+          fullPx += 17;
+        }
+
+        // TODO: 如果样式修改了fontSize，将不是7px。需要限制修改fontSize。
+        /** single character width is 7 when font size is 14px */
+        const px = stringWidth(u.label) * 7;
+        labelPx += px;
+        fullPx += px + marginLeft;
+      });
+
+    return [iconPx, labelPx, fullPx];
+  }
+
+  function computeHorizontal(rows: Rows) {
+    let iconPx = 0;
+    let labelPx = 0;
+    let fullPx = 0;
+
+    const rowPxs = (rows as Row[]).map((u) => {
+      let i = 0;
+      let l = 0;
+      let f = 0;
+      if (u.icon) {
+        i += 14;
+        f += 14;
+      }
+
+      l += stringWidth(u.label) * 7;
+      f += l + marginLeft;
+
+      return [i, l, f, i + l + f];
+    });
+
+    rowPxs.sort((a, b) => a[3] - b[3]);
+    const maxPx = rowPxs.pop();
+    iconPx = maxPx[iconIndex];
+    labelPx = maxPx[labelIndex];
+    fullPx = maxPx[fullIndex];
+
+    return [iconPx, labelPx, fullPx];
+  }
 
   useEffect(() => {
-    if (isRow(rows[0])) {
-      let iconPx = 0;
-      let labelPx = 0;
-      let fullPx = 0;
+    const groups = rows[0] instanceof Array ? rows : [rows];
 
-      if (type === "vertical") {
-        (rows as Row[])
-          .filter((u) => Object.keys(u).indexOf("visible") === -1 || u.visible)
-          .forEach((u, i) => {
-            /** icon width */
-            if (u.icon) {
-              iconPx += 14;
-              if (i > 0) iconPx += 17;
+    if (!isRow(groups[0][0])) return;
 
-              fullPx += 14;
-            }
+    const results =
+      type === "vertical"
+        ? groups.map((g: Rows) => computeVertical(g))
+        : groups.map((g: Rows) => computeHorizontal(g));
 
-            /** divider width */
-            if (i > 0) {
-              labelPx += 17;
-              fullPx += 17;
-            }
-
-            // TODO: 如果样式修改了fontSize，将不是7px。需要限制修改fontSize。
-            /** single character width is 7 when font size is 14px */
-            const px = stringWidth(u.label) * 7;
-            labelPx += px;
-            fullPx += px + marginLeft;
-          });
-      } else {
-        const rowPxs = (rows as Row[]).map((u) => {
-          let i = 0;
-          let l = 0;
-          let f = 0;
-          if (u.icon) {
-            i += 14;
-            f += 14;
-          }
-
-          l += stringWidth(u.label) * 7;
-          f += l + marginLeft;
-
-          return [i, l, f, i + l + f];
-        });
-
-        rowPxs.sort((a, b) => a[3] - b[3]);
-        const maxPx = rowPxs.pop();
-        iconPx = maxPx[0];
-        labelPx = maxPx[1];
-        fullPx = maxPx[2];
-      }
-
-      widthRef.current.icon = iconPx;
-      widthRef.current.label = labelPx;
-      widthRef.current.full = fullPx;
-    }
+    widthRef.current = results;
   }, []);
 
+  function getSizeType(pxs: number[], size: number) {
+    if (size > pxs[fullIndex]) {
+      return "full";
+    } else if (size > pxs[labelIndex] || pxs[iconIndex] === 0) {
+      return "label";
+    } else {
+      return "icon";
+    }
+  }
+
   useMemo(() => {
-    if (widthRef.current.full) {
-      if (size.width > widthRef.current.full) {
-        setSizeType("full");
-      } else if (size.width > widthRef.current.label || widthRef.current.icon === 0) {
-        setSizeType("label");
-      } else {
-        setSizeType("icon");
-      }
+    if (widthRef.current.length > 0) {
+      setSizeType(widthRef.current.map((u) => getSizeType(u, size.width)));
     }
   }, [size.width]);
 
-  const _dividerStyle: React.CSSProperties =
-    type === "horizontal" ? { margin: "1px 0px -2px 0px" } : {};
+  const defaultDividerStyle: React.CSSProperties =
+    type === "horizontal" ? { margin: "1px 0px" } : {};
 
-  const _style: React.CSSProperties =
-    type === "vertical" ? { display: "flex", justifyContent: "center" } : {};
+  const defaultOuterDividerStyle: React.CSSProperties =
+    type === "horizontal" ? { height: "auto" } : { margin: "2px 0px" };
+
+  const defaultStyle: React.CSSProperties =
+    type === "vertical" ? { display: "flex", justifyContent: "center", alignItems: "center" } : {};
+
+  const switchedType: DividerProps["type"] = type === "horizontal" ? "vertical" : "horizontal";
 
   const _rowStyle: Radium.StyleProps["rules"] = {
     ":hover": {
@@ -130,7 +166,7 @@ const Dividers: React.FC<DividerComponentProps> = ({
     ...rowStyle,
   };
 
-  function renderRow(row: React.ReactNode | Row) {
+  function renderRow(row: React.ReactNode | Row, groupIndex: number) {
     if (isRow(row)) {
       const node = (type: string) => (
         <Button
@@ -146,36 +182,59 @@ const Dividers: React.FC<DividerComponentProps> = ({
         </Button>
       );
 
-      if (sizeType === "icon") {
+      if (sizeType[groupIndex] === "icon") {
         if (row.icon) {
-          return <Tooltip title={row.label}>{node(sizeType)}</Tooltip>;
+          return <Tooltip title={row.label}>{node(sizeType[groupIndex])}</Tooltip>;
         } else {
           return node("label");
         }
       } else {
-        return node(sizeType);
+        return node(sizeType[groupIndex]);
       }
     } else {
       return row;
     }
   }
 
+  function renderItem(rows: Array<React.ReactNode | Row>, groupIndex: number) {
+    return rows
+      .filter((row) => !!row && (Object.keys(row).indexOf("visible") === -1 || row["visible"]))
+      .map((row, index) => {
+        return (
+          <>
+            {index !== 0 && dividerVisible && (
+              <Divider style={{ ...defaultDividerStyle, ...dividerStyle }} type={type} />
+            )}
+            <span style={_rowStyle} key={`${groupIndex}-node${index}`}>
+              {renderRow(row, groupIndex)}
+            </span>
+          </>
+        );
+      });
+  }
+
   return (
-    <div style={{ textAlign: "center", ..._style, ...style }} ref={divRef}>
-      {rows
-        .filter((row) => !!row && (Object.keys(row).indexOf("visible") === -1 || row["visible"]))
-        .map((row, index) => {
-          return (
-            <div key={index}>
-              {index !== 0 && dividerVisible && (
-                <Divider style={{ ..._dividerStyle, ...dividerStyle }} type={type} />
-              )}
-              <span style={_rowStyle} key={index}>
-                {renderRow(row)}
-              </span>
-            </div>
-          );
-        })}
+    <div
+      ref={divRef}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: type === "horizontal" ? "row" : "column",
+      }}
+    >
+      {(rows[0] instanceof Array ? rows : [rows]).map((u: Rows, i) => (
+        <>
+          {i !== 0 && outerDividerVisible && (
+            <Divider
+              style={{ ...defaultOuterDividerStyle, ...outerDividerStyle }}
+              type={switchedType}
+            />
+          )}
+          <div key={`row${i}`} style={{ ...defaultStyle, ...style }}>
+            {renderItem(u, i)}
+          </div>
+        </>
+      ))}
     </div>
   );
 };
