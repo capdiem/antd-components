f888af5bf9a12fa2af5bbdc2cf970576fdc159f9
capdiem <capdiem@live.com>
2021-01-11 22:27:39 +0800

    :wastebasket: remove: Remove unused Foo and update the index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export { default as Foo } from './Foo';
+import zhCN from "antd/es/locale/zh_CN";
+import { ConfigProviderProps } from "antd/lib/config-provider";
+
+import BlockText from "./block-text";
+// import Dividers from "./dividers";
+import EditableText from "./editable-text";
+// import Filter from "./filter";
+// import FormModal from "./form-modal";
+// import SubstringText from "./substring-text";
+// import Table from "./table";
+// import TableModal from "./table-modal";
+// import TableOperation from "./table-operation";
+
+let global: ConfigProviderProps = {
+  locale: zhCN,
+};
+
+function setConfigProviderProps(props: ConfigProviderProps) {
+  global = {
+    ...global,
+    ...props,
+  };
+}
+
+function getConfigProviderProps() {
+  return global;
+}
+
+export {
+  // SubstringText,
+  BlockText,
+  EditableText,
+  // Filter,
+  // TableOperation,
+  // FormModal,
+  // Table,
+  // TableModal,
+  // Dividers,
+  setConfigProviderProps,
+  getConfigProviderProps,
+};
+
+// export { default as Foo } from './Foo';
